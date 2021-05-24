538bc9f6fc0d9c680029b8cb977440ed61727954
dmitry <dimdroid2013@mail.ru>
2021-05-24 19:42:40 +0500

    Сделал декомпозицию функции рисования домика
    Создана функция рисования домика, как заготовка
    и документирован ее интерфейс.
    Если добавить ее реализацию, то программа заработает.

---
+++ homes.py
@@ -0,0 +1,18 @@
+def drow_house(x, y, width, height):
+    '''
+    Нарисовать домик ширины width и высоты height от опорной точки (x, y),
+    которая находится в середине нижней точки фундамента.
+    :param x: координата середины домика
+    :param y: координата низа фундамента
+    :param width: полная ширина домика (включая фундамент и вылеты крыши)
+    :param height: полная высота домика
+    :return: None
+    '''
+    print("Типа рисую домик ...", x, y, width, height)
+    pass # donoting
+
+x, y = 100, 100
+width, height = 200, 200
+
+drow_house(x, y, width, height)
+
